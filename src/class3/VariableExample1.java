ecbb2340d6e3dbd971a68432b1bea810e198026f
seungyeonchoi <bzt2971@gmail.com>
2020-04-07 03:21:33 +0900

    3주차 - 변수와 상수 예제1

---
+++ src/class3/VariableExample1.java
@@ -0,0 +1,16 @@
+package class3;
+
+public class VariableExample1 {
+    /*
+    * 3주차 - 변수와 상수 예제1
+    * 각종 자료형 사용해보기
+    */
+    public static void main(String[] args) {
+        int num = 123; //4byte 정수형
+        char c = 'a'; //2byte 문자형
+        float f = 3.14f; //4byte 실수
+        double d = 3.14; //8byte 실수
+        String str="Hello, MSC"; //문자열
+        System.out.println("num: "+num+", c: "+c+", f: "+f+", d: "+d+", str: "+str);
+    }
+}

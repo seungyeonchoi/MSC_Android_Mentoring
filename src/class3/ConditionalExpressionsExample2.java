19a56e42c814df051b99ad1a1e50304b35836594
seungyeonchoi <bzt2971@gmail.com>
2020-04-08 13:12:21 +0900

    3주차 - 조건 연산자 예제2

---
+++ src/class3/ConditionalExpressionsExample2.java
@@ -0,0 +1,16 @@
+package class3;
+
+public class ConditionalExpressionsExample2
+{
+    public static void main(String[] args) {
+        Boolean flag = true;
+        if(flag){
+            System.out.println("flag is true\n");
+        }
+        else {
+            System.out.println("flag is false\n");
+        }
+        if(3!=3) // (조건식) 이 참이면 실행됩니다.
+            System.out.println("\"3 is not 3\" is false ");
+    }
+}

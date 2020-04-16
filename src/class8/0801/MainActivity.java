ac1644561de20f2d9d00b1bee72febda2d5802c6
seungyeonchoi <48347010+seungyeonchoi@users.noreply.github.com>
2020-04-16 20:33:43 +0900

    Create MainActivity.java

---
+++ src/class8/0801/MainActivity.java
@@ -0,0 +1,23 @@
+package com.scjun.junny.app_0801;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view){
+        Intent intent = new Intent(this, WebViewPage.class);
+        startActivity(intent);
+    }
+}

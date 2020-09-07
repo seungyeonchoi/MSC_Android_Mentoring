e25c3e552c12ea5a1e1e4c358c4371e442f3b01b
seungyeonchoi <48347010+seungyeonchoi@users.noreply.github.com>
2020-09-07 22:38:57 +0900

    3주차 - 제어문 구조 예제4

---
+++ src/class3/ControlExpressionExample4.java
@@ -0,0 +1,27 @@
+package class3;
+
+public class ControlExpressionExample4 {
+    
+        char grade='B';
+        switch (grade)
+        {
+            case 'A':
+                System.out.println("91~100점 입니다.");
+                break;
+           case 'B':
+               System.out.println("81~90점 입니다.");
+               break;
+           case 'C':
+               System.out.println("71~80점 입니다.");
+               break;
+           case 'D':
+               System.out.println("61~70점 입니다.");
+               break;
+           case 'F':
+               System.out.println("60점 이하 입니다.");
+               break;
+           default:
+               System.out.println("잘못된 학점입니다.");
+        }
+     }
+}
